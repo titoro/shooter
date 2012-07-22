32611df9b27f257c2da9d22bbb911346e902314c
kamiyama <kamiyamaryuji@gmail.com>
2012-07-23 00:41:57 +0900

    bug-fixed: not implemented warning.

---
+++ shooter/AbstractStageObject.h
@@ -0,0 +1,14 @@
+//
+//  AbstractStageObject.h
+//  shooter
+//
+//  Created by ryuji kamiyama on 12/07/23.
+//  Copyright (c) 2012年 unko corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FrameProcessable.h"
+
+@interface AbstractStageObject : UIView <FrameProcessable>
+
+@end
